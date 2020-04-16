8bafe0e4a80da0ee13cb7cba8aabf2b5b4cf0a1e
caoji8 <www.972237177.com@qq.com>
2020-04-16 16:00:15 +0800

    4/16更新

---
+++ JS/syntax.js
@@ -0,0 +1,21 @@
+let str = 'This is the test string'
+let arr = ['小明', '小王']
+let obj = {
+    name: '小明',
+    age: 15
+}
+let json = [
+    { name: '小明' },
+    { name: '小王' }
+]
+// TODO: 对象不可迭代
+
+// for ( let {name} of json ) {
+//     console.log(name)
+// }
+// function iteration() {
+//     for ( let i of  arguments ) {
+//         console.log(i)
+//     }
+// }
+
